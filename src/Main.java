3da1408d59f01884bf0ef261153fec231be43296
Railya <fazyllinarailya@gmail.com>
2023-04-11 09:03:56 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13676;
+        int bonus = 20;
+
+        System.out.println("После покупки билета количество бонусных миль составило:");
+        System.out.println((amount / bonus) + "  полученные мили");
+    }
+}
